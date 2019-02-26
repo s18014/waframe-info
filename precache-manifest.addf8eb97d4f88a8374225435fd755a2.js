08ac2c121b484adbdce4617b757b07aef3960efe
s18014 <s18014@std.it-college.ac.jp>
2019-02-26 14:09:55 +0000

    Updates

---
+++ precache-manifest.addf8eb97d4f88a8374225435fd755a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d66b042a6cdb439a4cfa",
+    "url": "/waframe-info/static/css/main.fb22ac2b.chunk.css"
+  },
+  {
+    "revision": "d66b042a6cdb439a4cfa",
+    "url": "/waframe-info/static/js/main.d66b042a.chunk.js"
+  },
+  {
+    "revision": "22246b8e64af1f6eac4e",
+    "url": "/waframe-info/static/js/runtime~main.22246b8e.js"
+  },
+  {
+    "revision": "ce9877960e1736111b00",
+    "url": "/waframe-info/static/js/2.ce987796.chunk.js"
+  },
+  {
+    "revision": "a87599cca5453698cc57f44a1790586c",
+    "url": "/waframe-info/index.html"
+  }
+];
